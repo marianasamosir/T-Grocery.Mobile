4435bf559ff8cac6fd17d2e87d2e77942286693e
Mariana <marmar.smsr@gmail.com>
2024-04-26 21:41:21 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/org/d3if3159/t_grocery/MainActivity.kt
+++ app/src/main/java/org/d3if3159/t_grocery/MainActivity.kt
@@ -1,25 +1,16 @@
 package org.d3if3159.t_grocery
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import org.d3if3159.t_grocery.ui.screen.MainScreen
 import org.d3if3159.t_grocery.ui.theme.TGroceryTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,50 +23,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    MainScreen {modifier ->
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
-fun MainScreen(content: @Composable (Modifier) -> Unit) {
-    Scaffold (
-        topBar = {
-            TopAppBar(
-                navigationIcon = {
-                    Image(
-                        painter = painterResource(id = R.drawable.logo_tgrocery),
-                        contentDescription = stringResource(id = R.string.logo),
-                        modifier = Modifier.padding(start = 16.dp).size(100.dp)
-                    )
-                },
-                title = {},
-            )
-        }
-    ){padding ->
-        content(Modifier.padding(padding))
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 
 @Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun ScreenPreview() {
+fun GreetingPreview() {
     TGroceryTheme {
         Greeting("Android")
     }
 }
+
+
---
+++ app/src/main/java/org/d3if3159/t_grocery/ui/screen/MainScreen.kt
@@ -0,0 +1,64 @@
+package org.d3if3159.t_grocery.ui.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.d3if3159.t_grocery.R
+import org.d3if3159.t_grocery.ui.theme.TGroceryTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    Image(
+                        painter = painterResource(id = R.drawable.logo_tgrocery),
+                        contentDescription = stringResource(id = R.string.logo),
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .size(100.dp)
+                    )
+                },
+                title = {},
+            )
+        }
+    ){padding ->
+        ScreenContent(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun ScreenPreview() {
+    TGroceryTheme {
+        MainScreen()
+    }
+}
