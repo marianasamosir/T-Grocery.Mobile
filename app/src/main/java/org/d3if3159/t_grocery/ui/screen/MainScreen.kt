474dbfbd17ed4296009ad7caf6ead9a38400f570
Mariana <marmar.smsr@gmail.com>
2024-04-26 23:12:54 +0700

    Menambahkan Deskripsi

--- app/src/main/java/org/d3if3159/t_grocery/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3159/t_grocery/ui/screen/MainScreen.kt
@@ -1,20 +1,31 @@
 package org.d3if3159.t_grocery.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.d3if3159.t_grocery.R
 import org.d3if3159.t_grocery.ui.theme.TGroceryTheme
 
@@ -54,6 +65,30 @@ fun ScreenContent(modifier: Modifier) {
             contentDescription = "Ini gambar landing page",
 
         )
+        Column (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = stringResource(id = R.string.selamat_datang),
+                style = TextStyle(
+                    fontSize = 23.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .padding(top = 40.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.deskripsi),
+                style = TextStyle(fontSize = 13.sp),
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+
     }
 }
 
